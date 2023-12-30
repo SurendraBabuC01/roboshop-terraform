520be8c1404b455a0266bfcebaa8fd3e04343f55
surendra Babu C <surendra9703802865@gmail.com>
2023-12-30 14:49:07 +0530

    Terraform

--- servers.tf
+++ servers.tf
@@ -4,27 +4,65 @@ data "aws_ami" "centos" {
   name_regex  = "Centos-8-DevOps-Practice"
 }
 
-variable "instance_type" {
-  default = "t3.small"
-}
 
 data "aws_security_group" "allow-all" {
   name = "allow-all"
 }
 
 variable "components" {
-  default = ["frontend", "mongodb", "catalogue"]
+  default = {
+    frontend = {
+      name          = "frontend"
+      instance_type = "t3.small"
+    }
+    /*mongodb = {
+      name          = "mongodb"
+      instance_type = "t3.small"
+    }
+    catalogue = {
+      name          = "catalogue"
+      instance_type = "t3.micro"
+    }
+    redis = {
+      name          = "redis"
+      instance_type = "t3.small"
+    }
+    user = {
+      name          = "user"
+      instance_type = "t3.micro"
+    }
+    cart = {
+      name          = "cart"
+      instance_type = "t3.micro"
+    }
+    mysql = {
+      name          = "mysql"
+      instance_type = "t3.small"
+    }
+    shipping = {
+      name          = "shipping"
+      instance_type = "t3.medium"
+    }
+    rabbitmq = {
+      name          = "rabbitmq"
+      instance_type = "t3.small"
+    }
+    payment = {
+      name          = "payment"
+      instance_type = "t3.small"
+    }*/
+  }
 }
 
 
 resource "aws_instance" "instance" {
-  count                  = length(var.components)
+  for_each = var.components
   ami                    = data.aws_ami.centos.image_id
-  instance_type          = var.instance_type
+  instance_type          = each.value["instance_type"]
   vpc_security_group_ids = [data.aws_security_group.allow-all.id]
 
   tags = {
-    Name = var.components[count.index]
+    Name = each.value["name"]
   }
 }
 
