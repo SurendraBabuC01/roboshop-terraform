c6397205232a8860a0401064448d35acc0dcd0c6
surendra Babu C <surendra9703802865@gmail.com>
2023-12-30 14:54:16 +0530

    Terraform

--- servers.tf
+++ servers.tf
@@ -15,7 +15,7 @@ variable "components" {
       name          = "frontend"
       instance_type = "t3.small"
     }
-    /*mongodb = {
+  /*  mongodb = {
       name          = "mongodb"
       instance_type = "t3.small"
     }
@@ -56,7 +56,7 @@ variable "components" {
 
 
 resource "aws_instance" "instance" {
-  for_each = var.components
+  for_each               = var.components
   ami                    = data.aws_ami.centos.image_id
   instance_type          = each.value["instance_type"]
   vpc_security_group_ids = [data.aws_security_group.allow-all.id]
@@ -66,173 +66,12 @@ resource "aws_instance" "instance" {
   }
 }
 
-/*
-resource "aws_route53_record" "frontend" {
-  zone_id = "Z08550883SIRHNRK693H1"
-  name    = "frontend-dev"
-  type    = "A"
-  ttl     = 30
-  records = [aws_instance.frontend.private_ip]
+resource "aws_route53_record" "records" {
+  for_each = var.components
+  zone_id  = "Z08550883SIRHNRK693H1"
+  name     = "${each.value["name"]}-dev.surendrababuc01.online"
+  type     = "A"
+  ttl      = 30
+  records  = [aws_instance.instance.private_ip]
 }
 
-resource "aws_instance" "mongodb" {
-  ami                    = data.aws_ami.centos.image_id
-  instance_type          = "t3.micro"
-  vpc_security_group_ids = [data.aws_security_group.security_group.id]
-
-  tags = {
-    Name = "mongodb"
-  }
-}
-
-resource "aws_route53_record" "mongodb" {
-  zone_id = "Z08550883SIRHNRK693H1"
-  name    = "mongodb-dev"
-  type    = "A"
-  ttl     = 30
-  records = [aws_instance.mongodb.private_ip]
-}
-
-resource "aws_instance" "catalogue" {
-  ami                    = data.aws_ami.centos.image_id
-  instance_type          = var.instance_type
-  vpc_security_group_ids = [data.aws_security_group.security_group.id]
-
-  tags = {
-    Name = "catalogue"
-  }
-}
-
-resource "aws_route53_record" "catalogue" {
-  zone_id = "Z08550883SIRHNRK693H1"
-  name    = "catalogue-dev"
-  type    = "A"
-  ttl     = 30
-  records = [aws_instance.catalogue.private_ip]
-}
-
-resource "aws_instance" "redis" {
-  ami                    = data.aws_ami.centos.image_id
-  instance_type          = var.instance_type
-  vpc_security_group_ids = [data.aws_security_group.security_group.id]
-
-  tags = {
-    Name = "redis"
-  }
-}
-
-resource "aws_route53_record" "redis" {
-  zone_id = "Z08550883SIRHNRK693H1"
-  name    = "redis-dev"
-  type    = "A"
-  ttl     = 30
-  records = [aws_instance.redis.private_ip]
-}
-
-resource "aws_instance" "user" {
-  ami                    = data.aws_ami.centos.image_id
-  instance_type          = var.instance_type
-  vpc_security_group_ids = [data.aws_security_group.security_group.id]
-
-  tags = {
-    Name = "user"
-  }
-}
-
-resource "aws_route53_record" "user" {
-  zone_id = "Z08550883SIRHNRK693H1"
-  name    = "user-dev"
-  type    = "A"
-  ttl     = 30
-  records = [aws_instance.user.private_ip]
-}
-
-resource "aws_instance" "cart" {
-  ami                    = data.aws_ami.centos.image_id
-  instance_type          = var.instance_type
-  vpc_security_group_ids = [data.aws_security_group.security_group.id]
-
-  tags = {
-    Name = "cart"
-  }
-}
-
-resource "aws_route53_record" "cart" {
-  zone_id = "Z08550883SIRHNRK693H1"
-  name    = "cart-dev"
-  type    = "A"
-  ttl     = 30
-  records = [aws_instance.cart.private_ip]
-}
-
-resource "aws_instance" "mysql" {
-  ami                    = data.aws_ami.centos.image_id
-  instance_type          = var.instance_type
-  vpc_security_group_ids = [data.aws_security_group.security_group.id]
-
-  tags = {
-    Name = "mysql"
-  }
-}
-
-resource "aws_route53_record" "mysql" {
-  zone_id = "Z08550883SIRHNRK693H1"
-  name    = "mysql-dev"
-  type    = "A"
-  ttl     = 30
-  records = [aws_instance.mysql.private_ip]
-}
-
-resource "aws_instance" "shipping" {
-  ami                    = data.aws_ami.centos.image_id
-  instance_type          = var.instance_type
-  vpc_security_group_ids = [data.aws_security_group.security_group.id]
-
-  tags = {
-    Name = "shipping"
-  }
-}
-
-resource "aws_route53_record" "shipping" {
-  zone_id = "Z08550883SIRHNRK693H1"
-  name    = "shipping-dev"
-  type    = "A"
-  ttl     = 30
-  records = [aws_instance.shipping.private_ip]
-}
-
-resource "aws_instance" "rabbitmq" {
-  ami                    = data.aws_ami.centos.image_id
-  instance_type          = var.instance_type
-  vpc_security_group_ids = [data.aws_security_group.security_group.id]
-
-  tags = {
-    Name = "rabbitmq"
-  }
-}
-
-resource "aws_route53_record" "rabbitmq" {
-  zone_id = "Z08550883SIRHNRK693H1"
-  name    = "rabbitmq-dev"
-  type    = "A"
-  ttl     = 30
-  records = [aws_instance.rabbitmq.private_ip]
-}
-
-resource "aws_instance" "payment" {
-  ami                    = data.aws_ami.centos.image_id
-  instance_type          = var.instance_type
-  vpc_security_group_ids = [data.aws_security_group.security_group.id]
-
-  tags = {
-    Name = "payment"
-  }
-}
-
-resource "aws_route53_record" "payment" {
-  zone_id = "Z08550883SIRHNRK693H1"
-  name    = "payment-dev"
-  type    = "A"
-  ttl     = 30
-  records = [aws_instance.payment.private_ip]
-}*/
