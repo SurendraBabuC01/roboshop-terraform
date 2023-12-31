7a362172bcb89ca5b97b6e0ddfd224cda1db5551
surendra Babu C <surendra9703802865@gmail.com>
2024-01-01 02:12:57 +0530

    Terraform

--- servers.tf
+++ servers.tf
@@ -7,6 +7,23 @@ resource "aws_instance" "instance" {
   tags = {
     Name = each.value["name"]
   }
+
+  provisioner "remote-exec" {
+
+    connection {
+      type     = "ssh"
+      user     = "centos"
+      password = "DevOps321"
+      host     = self.private_ip
+    }
+
+    inline = [
+      "rm -rf roboshop-shell",
+      "git clone https://github.com/SurendraBabuC01/roboshop-shell.git",
+      "cd roboshop-shell",
+      "sudo bash ${each.value["name"]}.sh"
+    ]
+  }
 }
 
 resource "aws_route53_record" "records" {
