899b18a0bb803103afc983b15635c5863c50cee9
surendra Babu C <surendra9703802865@gmail.com>
2024-01-01 21:49:27 +0530

    Terraform

--- servers.tf
+++ servers.tf
@@ -1,8 +1,8 @@
 module "servers" {
-  for_each = var.components
-  source = "./module"
-  env = var.env
-  instance_type = each.value["instance_type"]
+  for_each       = var.components
+  source         = "./module"
+  env            = var.env
+  instance_type  = each.value["instance_type"]
   component_name = each.value["name"]
-  password = lookup(each.value, "password", "null")
+  password       = lookup(each.value, "password", "null")
 }
