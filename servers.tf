5a83fa2a9ecf831c24fa31fb8d5181a81c478530
surendra Babu C <surendra9703802865@gmail.com>
2024-01-01 19:56:26 +0530

    Terraform

--- servers.tf
+++ servers.tf
@@ -34,7 +34,7 @@ resource "null_resource" "provisioner" {
       "rm -rf roboshop-shell",
       "git clone https://github.com/SurendraBabuC01/roboshop-shell.git",
       "cd roboshop-shell",
-      "sudo bash ${each.value["name"]}.sh ${each.value["password"]}"
+      "sudo bash ${each.value["name"]}.sh ${lookup(each.value, "password", "null")}"
     ]
   }
 
