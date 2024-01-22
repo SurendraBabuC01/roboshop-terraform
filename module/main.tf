82c8d09091b5e6ba32c7d75a2d9c7ec5a64e6a60
surendra Babu C <surendra9703802865@gmail.com>
2024-01-22 23:42:42 +0530

    ansible

--- module/main.tf
+++ module/main.tf
@@ -65,24 +65,28 @@ resource "aws_iam_role_policy" "ssm-ps-policy" {
   role = aws_iam_role.role.id
 
   policy = jsonencode({
-    "Version" : "2012-10-17",
-    "Statement" : [
+    "Version": "2012-10-17",
+    "Statement": [
       {
-        "Sid" : "VisualEditor0",
-        "Effect" : "Allow",
-        "Action" : [
+        "Sid": "VisualEditor0",
+        "Effect": "Allow",
+        "Action": [
+          "kms:Decrypt",
           "ssm:GetParameterHistory",
           "ssm:GetParametersByPath",
           "ssm:GetParameters",
           "ssm:GetParameter"
         ],
-        "Resource" : "arn:aws:ssm:us-east-1:127710927797:parameter/${var.env}.${var.component_name}.*"
+        "Resource": [
+          "arn:aws:ssm:us-east-1:127710927797:parameter/${var.env}.${var.component_name}.*",
+          "arn:aws:kms:us-east-1:127710927797:key/f4c80366-50e1-4dbc-8d1a-102ebb740ae6"
+        ]
       },
       {
-        "Sid" : "VisualEditor1",
-        "Effect" : "Allow",
-        "Action" : "ssm:DescribeParameters",
-        "Resource" : "*"
+        "Sid": "VisualEditor1",
+        "Effect": "Allow",
+        "Action": "ssm:DescribeParameters",
+        "Resource": "*"
       }
     ]
   })
