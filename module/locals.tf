ad3d19bc025a919d0e4e1b361aab2b36984984a8
surendra Babu C <surendra9703802865@gmail.com>
2024-01-22 22:35:07 +0530

    ansible

--- module/locals.tf
+++ module/locals.tf
@@ -8,6 +8,6 @@ locals {
   ]
   app_commands = [
     "sudo labauto ansible",
-    "ansible-pull -i localhost, -U https://github.com/SurendraBabuC01/roboshop-ansible.git roboshop.yml -e role_name=${var.component_name} -e env=${env}"
+    "ansible-pull -i localhost, -U https://github.com/SurendraBabuC01/roboshop-ansible.git roboshop.yml -e role_name=${var.component_name} -e env=${var.env}"
   ]
 }
