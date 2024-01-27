bbd8a0cfedaa5dc3d6e2ffcad3aa21b1d73ce333
surendra Babu C <surendra9703802865@gmail.com>
2024-01-27 15:47:58 +0530

    ansible

--- module/locals.tf
+++ module/locals.tf
@@ -14,7 +14,9 @@ locals {
     Name = "${var.component_name}-${var.env}"
   }
   app_tags = {
-    Name    = "${var.component_name}-${var.env}"
-    Monitor = "true"
+    Name      = "${var.component_name}-${var.env}"
+    Monitor   = "true"
+    component = var.component_name
+    env       = var.env
   }
 }
