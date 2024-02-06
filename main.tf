008beed8e8a8af744274ed5c5aeab51617c472f7
surendra Babu C <surendra9703802865@gmail.com>
2024-02-06 23:53:44 +0530

    terraform

--- main.tf
+++ main.tf
@@ -37,6 +37,8 @@ module "docdb" {
   name           = each.value["name"]
   port_no        = each.value["port_no"]
   engine_version = each.value["engine_version"]
+  count          = each.value["count"]
+  instance_class = each.value["instance_class"]
   subnet_ids     = lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnets", null), each.value["subnet_name"], null), "subnet_ids", null)
   allow_db_cidr  = lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnets", null), each.value["allow_db_cidr"], null), "subnet_cidrs", null)
 
